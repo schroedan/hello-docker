fa0579400bf6f52d0011dc8f2fea1b12ce834f18
Daniel Schröder <daniel.schroeder@skriptfabrik.com>
2022-11-22 20:52:26 +0100

    Multiple Services

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,16 @@
+const dateFormat = require('date-format');
 const express = require('express');
 const gracefulShutdown = require('http-graceful-shutdown');
+const redisCache = require('express-redis-cache');
 
 const app = express();
+const cache = redisCache({ host: 'redis' });
 const port = 3000;
 
-app.get('/', (_, res) => {
-    res.send('Hello Docker!');
+app.get('/', cache.route({ expire: 10 }), (_, res) => {
+    res.send(
+        `Hello Docker! It is ${dateFormat.asString('hh:mm:ss', new Date())}.`,
+    );
 });
 
 gracefulShutdown(
